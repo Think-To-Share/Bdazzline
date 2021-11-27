36d61cdf96645d36dab72c0a986da5dcc0d9548e
Tulika Das <tulikadas1029@gmail.com>
2021-11-27 21:37:46 +0530

    footer section add slide add and some modify

--- src/js/_homepage.js
+++ src/js/_homepage.js
@@ -2,26 +2,26 @@ import { tns } from 'tiny-slider/src/tiny-slider'
 
 import GLightbox from 'glightbox';
 
-// if (document.querySelector('.hero-section')) {
-//     tns({
-//         container: '.hero-section .slides',
-//         items: 1,
-//         nav: false,
-//         // controlsContainer: '.hero-section .testimonial-nav',
-//         autoplay: true,
-//         controls:false,
-//         autoplayTimeout:50000,
-//         autoplayButtonOutput: false,
-//         controlsText:['<i class="fas fa-chevron-left"></i>','<i class="fas fa-chevron-right"></i>'],
-//         responsive: {
-//             768:{
-//               items: 1,
-//               controlsText:['<i class="fas fa-chevron-left"></i>','<i class="fas fa-chevron-right"></i>'],
-//               controls:true
-//             }
-//         }
-//     })
-// }
+if (document.querySelector('.hero-section')) {
+    tns({
+        container: '.hero-section .slides',
+        items: 1,
+        nav: false,
+        // controlsContainer: '.hero-section .testimonial-nav',
+        autoplay: true,
+        controls:false,
+        autoplayTimeout:50000,
+        autoplayButtonOutput: false,
+        controlsText:['<i class="fas fa-chevron-left"></i>','<i class="fas fa-chevron-right"></i>'],
+        responsive: {
+            768:{
+              items: 1,
+              controlsText:['<i class="fas fa-chevron-left"></i>','<i class="fas fa-chevron-right"></i>'],
+              controls:true
+            }
+        }
+    })
+}
 
 if (document.querySelector('.review-section')) {
     tns({
